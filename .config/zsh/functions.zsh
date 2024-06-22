b05b884581767bf2862139fefee8e45a70c39679
Marcos Cevallos <mjcevallos007@gmail.com>
2024-06-22 11:44:21 -0400

    Changing bat with cat to prevent other dependencies

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,6 +1,7 @@
 # Yazi
 function yy() {
 	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+
 	yazi "$@" --cwd-file="$tmp"
 	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
 		cd -- "$cwd"
@@ -8,4 +9,4 @@ function yy() {
 	rm -f -- "$tmp"
 }
 
-# Bat
+
