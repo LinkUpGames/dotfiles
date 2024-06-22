3ef347ee3306ac2fd29abce179818403819de87f
Marcos Cevallos <mjcevallos007@gmail.com>
2024-06-22 11:45:46 -0400

    nevermind that was just me

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,4 +1,4 @@
 alias vim=nvim
 alias fif="ssh -L 5173:localhost:5173 -L 5174:localhost:5174 -L 3000:localhost:3000 -L 8000:localhost:8000 -L 8080:localhost:8080 mac"
 alias ls='eza --color=always --no-filesize --no-time --no-permissions --icons=always --no-user --tree --level=1'
-alias bat='bat --paging=never --style="header, changes"'
+alias cat='bat --paging=never --style="header,changes"'
