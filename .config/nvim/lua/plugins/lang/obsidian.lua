d19b030b71dba1519b7544e0d0d9039cc67334c3
Marcos Cevallos <mjcevallos007@gmail.com>
2024-07-05 18:05:37 -0400

    Updated to include the obsidian plugin if we are looking at a markdown file

---
+++ .config/nvim/lua/plugins/lang/obsidian.lua
@@ -0,0 +1,35 @@
+local vaults = {
+  {
+    name = "school",
+    path = "/School/",
+  },
+}
+
+local events = {}
+local workspaces = {}
+
+for _, vault in ipairs(vaults) do
+  local BufReadPre = "BufReadPre " .. vim.fn.expand("~") .. vault.path .. "**.md"
+  local BufNewFile = "BufNewFile " .. vim.fn.expand("~") .. vault.path .. "**.md"
+
+  table.insert(events, BufReadPre)
+  table.insert(events, BufNewFile)
+
+  table.insert(workspaces, {
+    name = vault.name,
+    path = vim.fn.expand("~") .. vault.path,
+  })
+end
+
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  event = events,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = workspaces,
+  },
+}
