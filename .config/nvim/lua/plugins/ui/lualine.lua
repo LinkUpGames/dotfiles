c2e0c9b1c7599e339c0ce48efa3a1fcfbff6048e
Marcos Cevallos <mjcevallos007@gmail.com>
2024-08-04 00:24:29 -0400

    Updated the lualine to display oil in the status when it was opened

--- .config/nvim/lua/plugins/ui/lualine.lua
+++ .config/nvim/lua/plugins/ui/lualine.lua
@@ -1,25 +1,53 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  opts = function(_, opts)
-    -- Update the seperators
-    opts.options.component_separators = { left = "", right = "" }
-    opts.options.section_separators = { left = "", right = "" }
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      -- Update the seperators
+      opts.options.component_separators = { left = "", right = "" }
+      opts.options.section_separators = { left = "", right = "" }
 
-    opts.sections.lualine_y = {
-      {
-        "location",
-        icon = "󱇪",
-        padding = { left = 0, right = 1 },
-      },
-    }
+      opts.sections.lualine_y = {
+        {
+          "location",
+          icon = "󱇪",
+          padding = { left = 0, right = 1 },
+        },
+      }
 
-    opts.sections.lualine_z = {
-      function()
-        return " " .. os.date("%I:%M %p")
-      end,
-    }
+      opts.sections.lualine_z = {
+        function()
+          return " " .. os.date("%I:%M %p")
+        end,
+      }
 
-    -- Extensions
-    table.insert(opts.extensions, "oil")
-  end,
+      -- Extensions
+      local oil = {
+        sections = {
+          lualine_a = {
+            function()
+              return " OIL"
+            end,
+          },
+          lualine_b = {
+            function()
+              local ok, oil = pcall(require, "oil")
+
+              -- Load oil file
+              if ok then
+                ---@diagnostic disable-next-line: param-type-mismatch
+                return vim.fn.fnamemodify(oil.get_current_dir(), ":~")
+              else
+                return "ERROR"
+              end
+            end,
+          },
+        },
+        filetypes = {
+          "oil",
+        },
+      }
+
+      table.insert(opts.extensions, oil)
+    end,
+  },
 }
