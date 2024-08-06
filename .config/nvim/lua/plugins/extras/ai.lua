69853fc56485a70ef642c9201981c01ef85bb42e
marcosFIF <marcosc@fillitforward.com>
2024-08-06 08:11:07 -0400

    Added an LLM plugin support to mess around with in built chats

--- .config/nvim/lua/plugins/extras/ai.lua
+++ .config/nvim/lua/plugins/extras/ai.lua
@@ -2,4 +2,35 @@ return {
   { "Exafunction/codeium.nvim", lazy = true, opts = {
     enable_chat = true,
   }, enabled = false },
+  {
+    "robitx/gp.nvim",
+    config = function()
+      local opts = {
+        providers = {
+          googleai = {
+            disabled = false,
+            endpoint = "https://generativelanguage.googleapis.com/v1beta/models/{{model}}:streamGenerateContent?key={{secret}}",
+            secret = os.getenv("GEMINI_API_KEY"),
+          },
+        },
+
+        default_chat_agent = "Gemini",
+        default_command_agent = "Gemini",
+
+        agents = {
+          {
+            provider = "googleai",
+            name = "Gemini",
+            chat = true,
+            disable = false,
+            model = { model = "gemini-1.5-flash" },
+            system_prompt = require("gp.defaults").code_system_prompt,
+          },
+        },
+      }
+
+      require("gp").setup(opts)
+    end,
+    enabled = false,
+  },
 }
