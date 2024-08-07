73c77c519d49d9be225da4051fa5bd1ca0a0abfb
Marcos Cevallos <mjcevallos007@gmail.com>
2024-08-07 08:46:20 -0400

    Updates to the ai chat

--- .config/nvim/lua/plugins/extras/ai.lua
+++ .config/nvim/lua/plugins/extras/ai.lua
@@ -3,71 +3,40 @@ return {
     enable_chat = true,
   }, enabled = false },
   {
-    "robitx/gp.nvim",
-    config = function()
-      local opts = {
-        providers = {
-          googleai = {
-            disabled = false,
-            endpoint = "https://generativelanguage.googleapis.com/v1beta/models/{{model}}:streamGenerateContent?key={{secret}}",
-            secret = os.getenv("GEMINI_API_KEY"),
+    "frankroeder/parrot.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = "VeryLazy",
+    cond = os.getenv("GEMINI_API_KEY") ~= nil,
+    opts = {
+      chat_free_cursor = true,
+      toggle_target = "popup",
+      chat_shortcut_respond = { modes = { "n" }, shortcut = "<CR>" },
+      chat_shortcut_delete = { modes = { "n" }, shortcut = "<leader>ad" },
+      chat_confirm_delete = false,
+      providers = {
+        gemini = {
+          api_key = os.getenv("GEMINI_API_KEY"),
+        },
+      },
+      agents = {
+        chat = {
+          name = "Gemini",
+          model = {
+            model = "gemini-1.5-flash",
+            system_prompt = "You are helping me with my codebase",
+            provider = "gemini",
           },
         },
-
-        default_chat_agent = "Gemini",
-        default_command_agent = "Gemini",
-
-        agents = {
-          {
-            provider = "googleai",
-            name = "Gemini",
-            chat = true,
-            disable = false,
-            model = { model = "gemini-1.5-flash" },
-            system_prompt = require("gp.defaults").code_system_prompt,
-          },
-        },
-      }
-
-      require("gp").setup(opts)
-    end,
-    enabled = true,
+      },
+    },
     keys = {
+      { "<leader>aj", "<CMD>PrtChatToggle popup<CR>", mode = { "n" }, desc = "Toggle AI Chat" },
+      { "<leader>ak", "<CMD>PrtChatNew popup<CR>", mode = { "n" }, desc = "Open new AI Chat" },
       {
         "<leader>aj",
-        "<CMD>GpChatToggle split<CR>",
-        mode = "n",
-        desc = "Toggle AI Chat",
-      },
-      {
-        "<leader>ak",
-        ":GpChatNew split<CR>",
-        mode = "n",
-        desc = "Open a new AI Chat",
-      },
-      {
-        "<leader>aj",
-        ":'<,'>GpChatToggle split<CR>",
-        mode = { "x", "o" },
-        desc = "Toggle AI Chat",
-      },
-      {
-        "<leader>ak",
-        "<CMD>'<,'>GpChatNew split<CR>",
-        mode = { "x", "o" },
-        desc = "Open a new AI Chat",
-      },
-      {
-        "<leader>ad",
-        "<CMD>GpChatDelete<CR>",
-        mode = { "n" },
-        desc = "Delete the ai chat",
-      },
-      {
-        "<leader>a<CR>",
-        "<CMD>GpChatRespond<CR>",
-        mode = "n",
-        desc = "AI Chat Respond",
+        ":'<,'>PrtChatPaste popup<CR>",
+        mode = { "x" },
+        desc = "Paste the selected into the latest AI Chat",
       },
     },
   },
