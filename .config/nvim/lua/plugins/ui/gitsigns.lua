4e468deb02c23d2953f11f68bd2bdef0141cb154
Marcos Cevallos <mjcevallos007@gmail.com>
2024-08-08 19:39:49 -0400

    Updating gitsigns to include the preview inline for the latest commit in that line

---
+++ .config/nvim/lua/plugins/ui/gitsigns.lua
@@ -0,0 +1,7 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = "LazyFile",
+  keys = {
+    { "<leader>gp", "<CMD>Gitsigns toggle_current_line_blame<CR>", mode = { "n" }, desc = "Toggle Line Blame" },
+  },
+}
