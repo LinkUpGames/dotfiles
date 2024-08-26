99a683cbf3616fe59e271ff96f9f26f8ab2e806e
Marcos Cevallos <mjcevallos007@gmail.com>
2024-08-26 15:49:36 -0400

    Updating the fedora installation script

--- scripts/fedora_installation.sh
+++ scripts/fedora_installation.sh
@@ -2,32 +2,61 @@
 
 cd $HOME
 
+# Get the password first to download packages
+read -p "Enter SUDO Password: " password
+
 # Wezterm
-dnf copr enable wezfurlong/wezterm-nightly -y
-dnf install wezterm -y
+echo $password | sudo dnf copr enable wezfurlong/wezterm-nightly -y
+echo $password | sudo dnf install wezterm -y
 
 # Install zsh
-dnf install zsh -y
+echo $passowrd | sudo dnf install zsh -y
 
 # Make zsh default
-chsh -s $(which zsh)
+echo $password | chsh -s $(which zsh)
 
 # exec zsh
 
 # Zap plugin manager
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+
 cd $HOME
 rm -rf .zshrc
 
 # Tmux
-dnf install tmux -y
+echo $password | sudo dnf install tmux -y
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Neovim
-dnf install neovim -y
+echo $password | sudo dnf install neovim -y
 
 # fzf
-dnf install fzf -y
+echo $password | sudo dnf install fzf -y
+
+# fd-find
+echo $password | sudo dnf install fd-find -y
+
+# eza
+echo $password | sudo dnf install eza -y
+
+# lazygit
+echo $password | sudo dnf copr enable atim/lazygit -y
+echo $password | sudo dnf install lazygit -y
+
+# bat
+echo $password | sudo dnf install bat -y
+
+# Lazy docker
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# Stow
+echo $password | sudo dnf install stow -y
+
+# Switch Shell
+zsh
 
 # yazi
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -q -y
@@ -38,28 +67,6 @@ cargo install --locked yazi-fm yazi-cli
 
 # csvlens
 cargo install csvlens
-
-# fd-find
-dnf install fd-find -y
-
-# nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-
-# eza
-dnf install eza -y
-
-# lazygit
-dnf copr enable atim/lazygit -y
-dnf install lazygit -y
-
-# bat
-dnf install bat -y
-
-# Lazy docker
-curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
-
-# Stow
-dnf install stow -y
 
 ## Now link all the dependencies install
 cd $HOME
