3a9b96bca50dcb87f55e32e9ee986e78e9038242
Marcos Cevallos <multimarcxd@gmail.com>
2024-09-04 16:04:16 -0400

    Somehow got the lsp for gamemaker working so htat's awesome

--- .config/nvim/lua/plugins/lang/gml.lua
+++ .config/nvim/lua/plugins/lang/gml.lua
@@ -1,51 +1,64 @@
-if true then
-  return {}
-end
+vim.filetype.add({
+  extension = {
+    gml = "gml",
+  },
+})
 
--- return {
---
---   "neovim/nvim-lspconfig",
---   opts = {
---     servers = {
---       gml = {},
---     },
---     setup = {
---       gml = function(_, opts)
---         local lspconfig = require("lspconfig")
---         local configs = require("lspconfig.configs")
---         local util = require("lspconfig.util")
---
---         local root_files = {
---           ".gitignore",
---         }
---
---         if not configs.gml then
---           configs.gml = {
---             default_config = {
---               cmd = { "C:\\Program Files\\GameMaker Studio 2\\GameMakerLanguageServer.exe" },
---
---               filetypes = {
---                 "gml",
---                 "yyp",
---               },
---
---               single_file_support = true,
---
---               root_dir = util.root_pattern(unpack(root_files)),
---
---               settings = {},
---
---               commands = {},
---
---               docs = {
---                 description = [[]],
---               },
---             },
---           }
---         end
---
---         lspconfig.gml.setup(opts)
---       end,
---     },
---   },
--- }
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      gml = {},
+    },
+    setup = {
+      gml = function(_, opts)
+        local lspconfig = require("lspconfig")
+        local configs = require("lspconfig.configs")
+        local util = require("lspconfig.util")
+
+        local root_files = {
+          ".gitignore",
+        }
+
+        if not configs.gml then
+          configs.gml = {
+            default_config = {
+              cmd = { "C:\\Program Files\\GameMaker Studio 2\\GameMakerLanguageServer.exe", "--stdio" },
+
+              autostart = true,
+
+              filetypes = {
+                "gml",
+                "yyp",
+              },
+
+              single_file_support = true,
+
+              root_dir = util.root_pattern(unpack(root_files)),
+
+              settings = {},
+
+              init_options = {
+                runtimeDirectory = "C:\\ProgramData\\GameMakerStudio2\\Cache\\runtimes\\runtime-2024.8.0.216",
+                runtimeVersion = "2024.8.0.216",
+                platforms = { "windows" },
+                languagePacks = {
+                  "C:\\Program Files\\GameMaker Studio 2\\Plugins\\english\\english.csv",
+                },
+                language = "english",
+              },
+
+              commands = {},
+
+              docs = {
+                description = [[]],
+              },
+            },
+          }
+        end
+
+        lspconfig.gml.setup(opts)
+      end,
+    },
+  },
+}
