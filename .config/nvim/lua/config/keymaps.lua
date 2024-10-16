47532b4afac08ca619e0caa95ddd1a19eb0250df
Marcos Cevallos <mjcevallos007@gmail.com>
2024-10-16 11:56:50 -0400

    Updated some keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,8 +3,17 @@ local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 -- Basic keymaps
+-- Increment/Decrement
+keymap.set("n", "+", "<C-a>", opts)
+keymap.set("n", "-", "<C-x>", opts)
+
+-- Better Leave
 keymap.set("i", "jj", "<Esc>", opts) -- exit insert mode
-keymap.set("n", "<C-a>", "ggVG", opts)
+
+-- Select All
+keymap.set("n", "<C-a>", "gg<S-v>G", opts)
+
+-- Actual Tab
 keymap.set("n", "<Tab>", "<Tab>", opts) -- Remap tab to itself
-keymap.set("i", "<C-BS>", "<C-W>", opts)
+keymap.set("i", "<C-BS>", "<C-W>", opts) -- Delete words with backspace
 keymap.set("i", "<C-l>", "<Del>", opts) -- Forward delete character
