3e4c6d9e5c1b054e326388c249f3d80b608edad4
marcosFIF <marcosc@fillitforward.com>
2024-11-11 19:04:55 -0500

    updated bug

--- .config/nvim/lua/plugins/lang/godot.lua
+++ .config/nvim/lua/plugins/lang/godot.lua
@@ -12,7 +12,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      table.insert(opts.ensure_installed, {
+      vim.list_extend(opts.ensure_installed, {
         "gdscript",
       })
     end,
