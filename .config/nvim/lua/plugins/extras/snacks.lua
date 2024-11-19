d7b105bf5e474b4ba69cc62669174d3479402ebf
marcosFIF <marcosc@fillitforward.com>
2024-11-19 13:50:39 -0500

    shuffled dashboard around

--- .config/nvim/lua/plugins/extras/snacks.lua
+++ .config/nvim/lua/plugins/extras/snacks.lua
@@ -1,8 +1,55 @@
 return {
   "folke/snacks.nvim",
-  opts = {
-    notifier = {
-      enabled = true,
-    },
-  },
+
+  opts = function()
+    local logo = [[
+ _____                                                                                 _____ 
+( ___ )                                                                               ( ___ )
+ |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | 
+ |   | ███████╗ ██████╗ ██████╗██╗     ███████╗███████╗        █████╗     ██╗ ██████╗  |   | 
+ |   | ██╔════╝██╔════╝██╔════╝██║     ██╔════╝██╔════╝       ██╔══██╗██╗███║██╔═████╗ |   | 
+ |   | █████╗  ██║     ██║     ██║     █████╗  ███████╗       ╚██████║╚═╝╚██║██║██╔██║ |   | 
+ |   | ██╔══╝  ██║     ██║     ██║     ██╔══╝  ╚════██║        ╚═══██║██╗ ██║████╔╝██║ |   | 
+ |   | ███████╗╚██████╗╚██████╗███████╗███████╗███████║██╗     █████╔╝╚═╝ ██║╚██████╔╝ |   | 
+ |   | ╚══════╝ ╚═════╝ ╚═════╝╚══════╝╚══════╝╚══════╝╚═╝     ╚════╝     ╚═╝ ╚═════╝  |   | 
+ |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
+(_____)                                                                               (_____)
+Marcos Cevallos
+Development
+   ]]
+
+    local keys = {
+      { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+      { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+      {
+        icon = " ",
+        key = "c",
+        desc = "Config",
+        action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+      },
+      { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy },
+      { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+    }
+
+    return {
+      ---@type snacks.Config
+      notifier = {
+        enabled = true,
+      },
+      dashboard = {
+        -- width = 100,
+        enabled = true,
+        pane_gap = 1,
+        preset = {
+          header = logo,
+          keys = keys,
+        },
+        sections = {
+          { section = "header" },
+          { section = "keys", padding = 1, gap = 1 },
+          { section = "startup" },
+        },
+      },
+    }
+  end,
 }
