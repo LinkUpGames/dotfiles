8a1f6f2703776344c7e00fde0c59043d9f69b9a1
Marcos Cevallos <mjcevallos007@gmail.com>
2024-12-04 23:20:25 -0500

    working on some changes for the obsidian plugin

--- .config/nvim/lua/plugins/lang/obsidian.lua
+++ .config/nvim/lua/plugins/lang/obsidian.lua
@@ -21,10 +21,25 @@ for _, vault in ipairs(vaults) do
   })
 end
 
+table.insert(workspaces, {
+  name = "obsidian",
+  path = function()
+    return assert(vim.fn.getcwd())
+  end,
+  overrides = {
+    notes_subdir = vim.NIL,
+    new_notes_location = "current_dir",
+    templates = {
+      folder = vim.NIL,
+    },
+    disable_frontmatter = true,
+  },
+})
+
 return {
   "epwalsh/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
-  lazy = true,
+  lazy = false,
   event = events,
   dependencies = {
     "nvim-lua/plenary.nvim",
@@ -32,4 +47,9 @@ return {
   opts = {
     workspaces = workspaces,
   },
+  cond = function()
+    local obsidian_dir = vim.fn.finddir(".obsidian", vim.fn.getcwd())
+
+    return obsidian_dir ~= ""
+  end,
 }
