d01d79bd074037674b6d130ffc1301aec3e93ab7
Marcos Cevallos <mjcevallos007@gmail.com>
2024-12-07 15:00:42 -0500

    Updated obsidan plugin

--- .config/nvim/lua/plugins/lang/obsidian.lua
+++ .config/nvim/lua/plugins/lang/obsidian.lua
@@ -17,18 +17,44 @@ table.insert(workspaces, {
 })
 
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
-  lazy = false,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  opts = {
-    workspaces = workspaces,
-  },
-  cond = function() -- Only run in an obsidian based vault
-    local obsidian_dir = vim.fn.finddir(".obsidian", vim.fn.getcwd())
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = workspaces,
+    },
+    cond = function() -- Only run in an obsidian based vault
+      local obsidian_dir = vim.fn.finddir(".obsidian", vim.fn.getcwd())
 
-    return obsidian_dir ~= ""
-  end,
+      return obsidian_dir ~= ""
+    end,
+    keys = {
+      {
+        "<leader>op",
+        "<cmd>ObsidianPasteImg<cr>",
+        mode = { "n" },
+        ft = "markdown",
+        desc = "Paste the clipboard image to the markdown file",
+      },
+      {
+        "<leader>os",
+        "<cmd>ObsidianSearch<cr>",
+        mode = { "n" },
+        ft = "markdown",
+        desc = "Search for a specific file in Obsidian Vault",
+      },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>o", group = "Obsidian", icon = "󰆽" },
+      },
+    },
+  },
 }
