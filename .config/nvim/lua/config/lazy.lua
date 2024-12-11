6c31e2132f9a5b5c1e39896c401017863bb98df8
Marcos Cevallos <mjcevallos007@gmail.com>
2024-12-10 22:49:44 -0500

    Added blink cmp and trying it out

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     -- { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+    { import = "lazyvim.plugins.extras.coding.blink" },
 
     -- import/override with your plugins
 
