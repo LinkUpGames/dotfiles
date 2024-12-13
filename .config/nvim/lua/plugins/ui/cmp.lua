8e453388c9da227d4e0cd8602ae96a2fd2d1f6b4
Marcos Cevallos <mjcevallos007@gmail.com>
2024-12-13 13:07:43 -0500

    using fzf instead of telescope (for now)

--- .config/nvim/lua/plugins/ui/cmp.lua
+++ .config/nvim/lua/plugins/ui/cmp.lua
@@ -1,58 +1,58 @@
--- if true then
---   return {}
--- end
+if true then
+  return {}
+end
 
-return {
-  "hrsh7th/nvim-cmp",
-  event = { "InsertEnter", "CmdLineEnter" },
-  dependencies = {
-    "hrsh7th/cmp-cmdline",
-  },
-  lazy = true,
-  opts = function(_, _)
-    local cmp = require("cmp")
-    -- `:` cmdline setup.
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline({
-        ["<CR>"] = {
-          c = function(default)
-            if cmp.visible() then
-              return cmp.confirm({ select = true })
-            end
-
-            default()
-          end,
-        },
-      }),
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        {
-          name = "cmdline",
-          option = {
-            ignore_cmds = { "Man", "!" },
-          },
-        },
-      }),
-    })
-
-    -- '/' cmdline setup
-    cmp.setup.cmdline("/", {
-      mapping = cmp.mapping.preset.cmdline({
-        ["<CR>"] = {
-          c = function(default)
-            if cmp.visible() then
-              return cmp.confirm({ select = true })
-            end
-
-            default()
-          end,
-        },
-      }),
-      sources = {
-        { name = "buffer" },
-      },
-    })
-  end,
-  enabled = false,
-}
+-- return {
+--   "hrsh7th/nvim-cmp",
+--   event = { "InsertEnter", "CmdLineEnter" },
+--   dependencies = {
+--     "hrsh7th/cmp-cmdline",
+--   },
+--   lazy = true,
+--   opts = function(_, _)
+--     local cmp = require("cmp")
+--     -- `:` cmdline setup.
+--     cmp.setup.cmdline(":", {
+--       mapping = cmp.mapping.preset.cmdline({
+--         ["<CR>"] = {
+--           c = function(default)
+--             if cmp.visible() then
+--               return cmp.confirm({ select = true })
+--             end
+--
+--             default()
+--           end,
+--         },
+--       }),
+--       sources = cmp.config.sources({
+--         { name = "path" },
+--       }, {
+--         {
+--           name = "cmdline",
+--           option = {
+--             ignore_cmds = { "Man", "!" },
+--           },
+--         },
+--       }),
+--     })
+--
+--     -- '/' cmdline setup
+--     cmp.setup.cmdline("/", {
+--       mapping = cmp.mapping.preset.cmdline({
+--         ["<CR>"] = {
+--           c = function(default)
+--             if cmp.visible() then
+--               return cmp.confirm({ select = true })
+--             end
+--
+--             default()
+--           end,
+--         },
+--       }),
+--       sources = {
+--         { name = "buffer" },
+--       },
+--     })
+--   end,
+--   enabled = false,
+-- }
--- .config/nvim/lua/plugins/ui/telescope.lua
+++ .config/nvim/lua/plugins/ui/telescope.lua
@@ -1,15 +1,19 @@
-return {
-  "nvim-telescope/telescope.nvim",
-  opts = {
-    pickers = {
-      find_files = {
-        hidden = true,
-      },
-    },
-    defaults = {
-      path_display = {
-        "filename_first",
-      },
-    },
-  },
-}
+if true then
+  return {}
+end
+
+-- return {
+--   "nvim-telescope/telescope.nvim",
+--   opts = {
+--     pickers = {
+--       find_files = {
+--         hidden = true,
+--       },
+--     },
+--     defaults = {
+--       path_display = {
+--         "filename_first",
+--       },
+--     },
+--   },
+-- }
