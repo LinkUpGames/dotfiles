ba6babab91d6e43985ad7d629567075b46239fe0
Marcos Cevallos <mjcevallos007@gmail.com>
2025-01-19 23:24:49 -0500

    updated java format

--- .config/nvim/lua/plugins/lang/java.lua
+++ .config/nvim/lua/plugins/lang/java.lua
@@ -5,6 +5,19 @@ return {
       jdtls = function(opts)
         opts.dap_main = false
 
+        opts.settings = {
+          java = {
+            format = {
+              enabled = true,
+            },
+            inlayHints = {
+              parameterNames = {
+                enabled = "all",
+              },
+            },
+          },
+        }
+
         return opts
       end,
     },
