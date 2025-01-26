6094c117558f806d03bba5a346e0e9743cdffd8a
Anonymous <mjcevallos007@gmail.com>
2025-01-25 23:20:02 -0500

    i do want to see size

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,5 +1,5 @@
 alias vim=nvim
 alias v=nvim
 alias fif="ssh -L 5173:localhost:5173 -L 5174:localhost:5174 -L 3000:localhost:3000 -L 8000:localhost:8000 -L 3002:localhost:3002 -L 8080:localhost:8080 mac"
-alias ls='eza --color=always --no-filesize --icons=always --no-user'
+alias ls='eza --color=always --icons=always --no-user'
 alias cat='bat --paging=never --style="header,changes,numbers" --theme="ansi"'
