ec0274e052f0f3f78e383e0d70d1040a43416d28
Anonymous <mjcevallos007@gmail.com>
2025-01-25 22:57:44 -0500

    updated nvidia script

--- .config/rofi/scripts/nvidia.sh
+++ .config/rofi/scripts/nvidia.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-__NV_PRIME_RENDER_OFFLOAD=1 rofi -show combi -combi-modes "drun,run" -modes combi
+__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia rofi -show combi -combi-modes "drun,run" -modes combi
