f53420c52a755cfed43613dc26325204740609fd
marcosFIF <marcosc@fillitforward.com>
2025-01-29 09:00:05 -0500

    updated blink opts

--- .config/nvim/lua/plugins/code/blink.lua
+++ .config/nvim/lua/plugins/code/blink.lua
@@ -1,41 +1,35 @@
 return {
   "Saghen/blink.cmp",
+  opts = {
+    sources = {
+      cmdline = function()
+        local type = vim.fn.getcmdtype()
 
-  opts = function(_, opts)
-    opts.sources.cmdline = function()
-      local type = vim.fn.getcmdtype()
+        if type == "/" or type == "?" then
+          return { "buffer" }
+        end
 
-      if type == "/" or type == "?" then
-        return { "buffer" }
-      end
-
-      if type == ":" then
-        return { "cmdline" }
-      end
-
-      return {}
-    end
-
-    opts.sources.min_keyword_length = function()
-      return vim.bo.filetype == "markdown" and 3 or 0
-    end
-
-    -- Don't show until shown in cmd_line
-    opts.completion = {
+        if type == ":" then
+          return { "cmdline" }
+        end
+      end,
+      min_keyword_length = function()
+        return vim.bo.filetype == "markdown" and 3 or 0
+      end,
+    },
+    completion = {
       menu = {
         auto_show = function(ctx)
           return ctx.mode ~= "cmdline" and not vim.tbl_contains({ "/", "?" }, vim.fn.getcmdtype())
         end,
       },
-    }
-
-    -- Add the cmdline keymaps to run only when wanted
-    opts.keymap.cmdline = {
-      preset = "enter",
-      ["<C-space>"] = {},
-      ["<Tab>"] = { "show", "hide", "show_documentation", "hide_documentation" },
-    }
-
-    return opts
-  end,
+    },
+    keymap = {
+      cmdline = {
+        preset = "enter",
+        ["<C-space>"] = {},
+        ["<Tab>"] = { "show", "hide", "show_documentation", "hide_documentation" },
+      },
+    },
+  },
 }
