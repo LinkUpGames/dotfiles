807527f6abbf000caa8033d520756fb404be22ab
Anonymous <mjcevallos007@gmail.com>
2025-02-08 18:19:41 -0500

    traded with rosyln for now in c#

--- .config/nvim/lua/plugins/lang/c#.lua
+++ .config/nvim/lua/plugins/lang/c#.lua
@@ -1,31 +1,36 @@
 return {
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       omnisharp = {
+  --         handlers = {
+  --           ["textDocument/definition"] = function(...)
+  --             return require("omnisharp_extended").handler(...)
+  --           end,
+  --         },
+  --         keys = {
+  --           {
+  --             "gd",
+  --             function()
+  --               require("omnisharp_extended").lsp_definitions()
+  --             end,
+  --             desc = "Goto Definition",
+  --           },
+  --         },
+  --         enable_roslyn_analyzers = true,
+  --         organize_imports_on_format = true,
+  --         enable_import_completion = true,
+  --       },
+  --     },
+  --   },
+  -- },
+  -- { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
   {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        omnisharp = {
-          handlers = {
-            ["textDocument/definition"] = function(...)
-              return require("omnisharp_extended").handler(...)
-            end,
-          },
-          keys = {
-            {
-              "gd",
-              function()
-                require("omnisharp_extended").lsp_definitions()
-              end,
-              desc = "Goto Definition",
-            },
-          },
-          enable_roslyn_analyzers = true,
-          organize_imports_on_format = true,
-          enable_import_completion = true,
-        },
-      },
-    },
+    "seblyng/roslyn.nvim",
+    ft = "cs",
+    opts = {},
   },
-  { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
   {
     "stevearc/conform.nvim",
     opts = {
