ec6942a2adc251eba7eb092a926efaaf6d368e28
marcosFIF <marcosc@fillitforward.com>
2025-02-10 09:50:11 -0500

    I want inlay hints sometimes

--- .config/nvim/lua/plugins/lang/javascript.lua
+++ .config/nvim/lua/plugins/lang/javascript.lua
@@ -33,6 +33,12 @@ return {
   {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
+    opts = {
+      settings = {
+        tsserver_file_preferences = {
+          includeInlayParameterNameHints = "all",
+        },
+      },
+    },
   },
 }
