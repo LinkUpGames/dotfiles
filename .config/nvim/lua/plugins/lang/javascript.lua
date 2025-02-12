7dc5df4c936101d3962e116d56c2120b23a0a652
marcosFIF <marcosc@fillitforward.com>
2025-02-12 13:38:34 -0500

    for some reason the inlay hints cause lag

--- .config/nvim/lua/plugins/lang/javascript.lua
+++ .config/nvim/lua/plugins/lang/javascript.lua
@@ -55,11 +55,11 @@ return {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {
-      settings = {
-        tsserver_file_preferences = {
-          includeInlayParameterNameHints = "all",
-        },
-      },
+      --   settings = {
+      --     tsserver_file_preferences = {
+      --       includeInlayParameterNameHints = "all",
+      --     },
+      --   },
     },
   },
 }
