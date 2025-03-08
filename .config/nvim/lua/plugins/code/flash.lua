9c1ca05e5bc659570798ad65ce5f8f9faf735411
Anonymous <mjcevallos007@gmail.com>
2025-03-07 20:39:05 -0500

    Updated flash for pickers

--- .config/nvim/lua/plugins/code/flash.lua
+++ .config/nvim/lua/plugins/code/flash.lua
@@ -1,58 +1,76 @@
 return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  enabled = true,
-  opts = {
-    modes = {
-      char = {
-        enabled = false,
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    enabled = true,
+    opts = {
+      modes = {
+        char = {
+          enabled = false,
+        },
+        search = {
+          enabled = true,
+        },
       },
-      search = {
-        enabled = true,
+    },
+    keys = {
+      {
+        "t",
+        function()
+          require("flash").jump()
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Jump To",
+      },
+      {
+        "T",
+        function()
+          require("flash").treesitter()
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Jump Treesitter",
+      },
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        false,
+      },
+      {
+        "S",
+        mode = { "n", "x", "o" },
+        false,
+      },
+      {
+        "r",
+        mode = { "n", "x", "o" },
+        false,
+      },
+      {
+        "R",
+        mode = { "n", "x", "o" },
+        false,
+      },
+      {
+        "<c-s>",
+        mode = { "n", "x", "o" },
+        false,
       },
     },
   },
-  keys = {
-    {
-      "t",
-      function()
-        require("flash").jump()
-      end,
-      mode = { "n", "x", "o" },
-      desc = "Jump To",
-    },
-    {
-      "T",
-      function()
-        require("flash").treesitter()
-      end,
-      mode = { "n", "x", "o" },
-      desc = "Jump Treesitter",
-    },
-    {
-      "s",
-      mode = { "n", "x", "o" },
-      false,
-    },
-    {
-      "S",
-      mode = { "n", "x", "o" },
-      false,
-    },
-    {
-      "r",
-      mode = { "n", "x", "o" },
-      false,
-    },
-    {
-      "R",
-      mode = { "n", "x", "o" },
-      false,
-    },
-    {
-      "<c-s>",
-      mode = { "n", "x", "o" },
-      false,
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        win = {
+          input = {
+            keys = {
+              ["<a-s>"] = false,
+              ["s"] = false,
+              ["t"] = { "flash", mode = { "n" } },
+            },
+          },
+        },
+      },
     },
   },
 }
