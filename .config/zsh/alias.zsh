cddf3d6057f568c60daaefc8f0a46503d17f7f82
Anonymous <mjcevallos007@gmail.com>
2025-03-26 14:19:53 -0400

    added a new alias for iterating over directories using fxf

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -3,3 +3,4 @@ alias v=nvim
 alias fif="ssh -L 5173:localhost:5173 -L 5174:localhost:5174 -L 3000:localhost:3000 -L 8000:localhost:8000 -L 3002:localhost:3002 -L 8080:localhost:8080 mac"
 alias ls='eza --color=always --icons=always --no-user'
 alias cat='bat --paging=never --style="header,changes,numbers" --theme="ansi"'
+alias cdf='cd ./$(fd . --type d --hidden --max-depth 5 | fzf --layout reverse --border)'
