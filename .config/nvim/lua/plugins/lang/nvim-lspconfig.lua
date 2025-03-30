31fb46b71b0050177d68c405e70daa9c5198f731
marcosFIF <marcosc@fillitforward.com>
2025-03-30 19:33:34 -0400

    Updating current diagnostics

--- .config/nvim/lua/plugins/lang/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/lang/nvim-lspconfig.lua
@@ -6,10 +6,13 @@ return {
     -- NOTE: This is to support virtual lines
     if vim.fn.has("nvim-0.11") == 1 then
       opts.diagnostics.virtual_lines = {
-        current_line = true,
+        current_line = false,
+        prefix = "●",
       }
 
-      opts.diagnostics.virtual_text = false
+      opts.diagnostics.virtual_text = {
+        current_line = true,
+      }
     else
       opts.diagnostics.virtual_text = true
     end
