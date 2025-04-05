47ed00ded9cb36ce848a8eb8efdc9c76ef1a07e4
Anonymous <mjcevallos007@gmail.com>
2025-04-05 16:15:17 -0400

    added an extra check until fedora updates the nvim repo

--- .config/nvim/lua/plugins/extras/visual-space.lua
+++ .config/nvim/lua/plugins/extras/visual-space.lua
@@ -3,5 +3,11 @@ return {
   "mcauley-penney/visual-whitespace.nvim",
   config = true,
   keys = { "v", "V", "<C-v>" }, -- optionally, lazy load on visual mode keys,
-  enabled = false,
+  enabled = function()
+    if vim.fn.has("nvim-0.11") == 1 then
+      return true
+    end
+
+    return false
+  end,
 }
