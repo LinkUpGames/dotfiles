648d94ec16d3e196716d0cbcc1aa3f011925c8a6
Anonymous <mjcevallos007@gmail.com>
2025-04-05 14:22:01 -0400

    added a new plugin for visual whitespace
    The plugin is disabled until the neovim version is update

---
+++ .config/nvim/lua/plugins/extras/visual-space.lua
@@ -0,0 +1,7 @@
+-- This is something to add once fedora updates there package repo
+return {
+  "mcauley-penney/visual-whitespace.nvim",
+  config = true,
+  keys = { "v", "V", "<C-v>" }, -- optionally, lazy load on visual mode keys,
+  enabled = false,
+}
