bf655e6c53af3237cf44b61ad3ee6895189e6a4b
marcosFIF <marcosc@fillitforward.com>
2025-04-10 19:36:30 -0400

    using my own plugin now for randomizing themes

--- .config/nvim/lua/plugins/ui/colorscheme.lua
+++ .config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,131 +1,3 @@
--- Themes that we can use
-local themes = {
-  "eldritch",
-  "tokyonight",
-  "catppuccin",
-  "vscode",
-  "cyberdream",
-  "fluoromachine",
-  "bamboo-multiplex",
-}
-
----Save the colorscheme and date to a save file
----@param colorscheme string The colorscheme to save
----@param date string The date saved as yyyy-mm-dd
-local save_file = function(colorscheme, date)
-  local file = io.open(vim.fn.stdpath("data") .. "/colorscheme", "w+")
-
-  if file then
-    file:write(colorscheme, "\n")
-    file:write(date, "\n")
-    file:close()
-  end
-end
-
----Checks the current file and returns the date and colorscheme saved
----@return table The table contains the colorscheme and date
-local check_file = function()
-  local table = {
-    colorscheme = "",
-    date = "",
-  }
-
-  local file = io.open(vim.fn.stdpath("data") .. "/colorscheme", "r")
-  if file then
-    local colorscheme, date = file:read("*l"), file:read("*l")
-
-    table.colorscheme = colorscheme
-    table.date = date
-  end
-
-  return table
-end
-
----Return the date as a table with the year, month and day
----@param date_string any
-local parse_date = function(date_string)
-  -- Split the date string by hyphens
-  local year, month, day = date_string:match("(%d+)-(%d+)-(%d+)")
-
-  -- Convert the parts to integers
-  year = tonumber(year)
-  month = tonumber(month)
-  day = tonumber(day)
-
-  -- Return the year, month, and day
-  return { year = year, month = month, day = day }
-end
-
----Compares a previous and current date and checks if the current date is more than a day away
----@param current any
----@param previous any
-local next_day = function(current, previous)
-  local current_date = parse_date(current)
-  local previous_date = parse_date(previous)
-
-  if current_date.year > previous_date.year then -- Different Years
-    return true
-  elseif current_date.month > previous_date.month then -- Different months
-    return true
-  else
-    if current_date.day > previous_date.day then
-      return true
-    end
-  end
-
-  return false
-end
-
----Returns a random colorscheme from the themes table
----@return string The colorscheme
-local random_theme = function()
-  local i = math.random(os.time()) % #themes
-  i = i == 0 and #themes or i
-
-  return themes[i]
-end
-
----Get a colorschema from the table that does not match the one provided
----@param colorscheme string The colorscheme
-local get_truly_random = function(colorscheme)
-  local newcolorscheme
-  repeat
-    newcolorscheme = random_theme()
-  until colorscheme ~= newcolorscheme
-
-  return newcolorscheme
-end
-
----Get the theme that is in the saved file and check if it's the next day
----@return string colorschem The colorscheme
-local get_theme = function()
-  local colorscheme
-  local date = os.date("%Y-%m-%d")
-
-  -- Check the saved file
-  local data = check_file()
-
-  if data.date ~= nil and data.date ~= "" then -- Save file exists
-    local savedcolorscheme = data.colorscheme
-    -- Check if it's the next day and get new theme
-    if next_day(date, data.date) then
-      colorscheme = get_truly_random(savedcolorscheme)
-
-      save_file(colorscheme, tostring(date))
-    else
-      colorscheme = savedcolorscheme
-    end
-  else -- Create a saved file
-    colorscheme = random_theme()
-    save_file(colorscheme, tostring(date))
-  end
-
-  return colorscheme
-end
-
--- Get the current theme for the day
-local theme = get_theme()
-
 return {
   {
     "eldritch-theme/eldritch.nvim",
@@ -388,12 +260,16 @@ return {
   -- },
   {
     "LinkUpGames/jumble.nvim",
-    opts = {},
-  },
-  {
-    "LazyVim/LazyVim",
     opts = {
-      colorscheme = theme,
+      themes = {
+        "eldritch",
+        "tokyonight",
+        "catppuccin",
+        "vscode",
+        "cyberdream",
+        "fluoromachine",
+        "bamboo-multiplex",
+      },
     },
   },
 }
