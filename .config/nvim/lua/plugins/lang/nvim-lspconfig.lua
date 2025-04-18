73a3ab29be898487c41e9d233aac2740f06ff938
Anonymous <mjcevallos007@gmail.com>
2025-04-18 19:44:31 -0400

    Updated so that all versions have the new version of nvim

--- .config/nvim/lua/plugins/lang/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/lang/nvim-lspconfig.lua
@@ -3,20 +3,17 @@ return {
   opts = function(_, opts)
     opts.inlay_hints.enabled = true
 
-    -- NOTE: This is to support virtual lines
-    if vim.fn.has("nvim-0.11") == 1 then
-      opts.diagnostics.virtual_lines = {
-        current_line = false,
-        prefix = "●",
-        severity = {
-          min = vim.diagnostic.severity.ERROR,
-        },
-      }
+    opts.diagnostics.virtual_lines = {
+      current_line = false,
+      prefix = "●",
+      severity = {
+        min = vim.diagnostic.severity.ERROR,
+      },
+    }
 
-      opts.diagnostics.virtual_text.severity = {
-        max = vim.diagnostic.severity.WARN,
-      }
-    end
+    opts.diagnostics.virtual_text.severity = {
+      max = vim.diagnostic.severity.WARN,
+    }
 
     return opts
   end,
