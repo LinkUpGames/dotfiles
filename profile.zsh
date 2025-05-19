5b6d6373303d9f8b07c1a3d509114a25da78b8e6
Anonymous <mjcevallos007@gmail.com>
2025-05-19 00:11:20 -0400

    removed .zprofile and .zshrc since we'll be sourcing another

--- profile.zsh
+++ profile.zsh
@@ -1,5 +1,12 @@
 # Entry Point
 
+# # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 # Macos defaults to brew
 if [ $(uname) = "Darwin" ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
@@ -25,3 +32,6 @@ plug "$HOME/.config/zsh/*"
 # Load and initialise completion system
 autoload -Uz compinit
 compinit
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
