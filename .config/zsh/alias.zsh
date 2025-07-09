94cbc06d54ca7eaef0044af9869fc36766da4ff1
marcosFIF <marcosc@fillitforward.com>
2025-07-09 07:13:21 -0400

    Updated to television

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -3,4 +3,5 @@ alias v=nvim
 alias fif="ssh -L 5173:localhost:5173 -L 5174:localhost:5174 -L 3000:localhost:3000 -L 8000:localhost:8000 -L 3002:localhost:3002 -L 8080:localhost:8080 mac"
 alias ls='eza --color=always --icons=always --no-user'
 alias cat='bat --paging=never --style="header,changes,numbers" --theme="ansi"'
-alias cdf='cd ./$(fd . --type d --hidden --max-depth 6 --exclude .git --exclude node_modules --exclude .cache | fzf --layout reverse --border)'
+#alias cdf='cd ./$(fd . --type d --hidden --max-depth 6 --exclude .git --exclude node_modules --exclude .cache | fzf --layout reverse --border)'
+alias cdf='tv dirs'
