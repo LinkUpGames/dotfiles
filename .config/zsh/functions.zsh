563ffc5460e11499848da3e62984697267934d2c
Anonymous <mjcevallos007@gmail.com>
2025-07-11 14:41:19 -0400

    Updated zsh functions

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -10,11 +10,27 @@ function yy() {
 }
 
 # Television
-function cdf() {
+# Go to a directory
+function cdd() {
   local dir=$(tv dirs)
 
   # Only go to output if the result is greater than 0
   if [[ -n "$dir" ]]; then
     cd "$dir"
   fi
+}
+
+# Open a file in a directory
+function cdf() {
+  local file=$(tv)
+  local dir=$(dirname $file)
+
+  echo $dir
+
+  if [[ -n "$dir" ]]; then
+    cd "$dir"
+
+    nvim $file
+  fi
+
 }
