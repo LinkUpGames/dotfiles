7f6f3c2977d710af1fa669a04bdb8e183c362013
Anonymous <mjcevallos007@gmail.com>
2025-07-28 21:15:06 -0400

    added a very basic version of diffview

--- .config/nvim/lua/plugins/ui/diffview.lua
+++ .config/nvim/lua/plugins/ui/diffview.lua
@@ -2,6 +2,21 @@ return {
   {
     "sindrets/diffview.nvim",
     opts = {},
-    enabled = false,
+    keys = {
+      {
+        "<leader>gD",
+        function()
+          local lib = require("diffview.lib")
+
+          if lib.get_current_view() then
+            vim.cmd("DiffviewClose")
+          else
+            vim.cmd("DiffviewOpen")
+          end
+        end,
+        mode = { "n" },
+        desc = "Toggle Diff View",
+      },
+    },
   },
 }
