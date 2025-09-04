3458a78f99e259842a9a703939d25da873ce47b1
Anonymous <mjcevallos007@gmail.com>
2025-09-03 23:29:03 -0400

    added snacks

--- .config/nvim/lua/plugins/code/snacks.lua
+++ .config/nvim/lua/plugins/code/snacks.lua
@@ -1,6 +1,116 @@
 return {
   "folke/snacks.nvim",
-  opts = function() end,
+  opts = function()
+    local logo = [[
+ _____                                                                                 _____ 
+( ___ )                                                                               ( ___ )
+ |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | 
+ |   | ███████╗ ██████╗ ██████╗██╗     ███████╗███████╗        █████╗     ██╗ ██████╗  |   | 
+ |   | ██╔════╝██╔════╝██╔════╝██║     ██╔════╝██╔════╝       ██╔══██╗██╗███║██╔═████╗ |   | 
+ |   | █████╗  ██║     ██║     ██║     █████╗  ███████╗       ╚██████║╚═╝╚██║██║██╔██║ |   | 
+ |   | ██╔══╝  ██║     ██║     ██║     ██╔══╝  ╚════██║        ╚═══██║██╗ ██║████╔╝██║ |   | 
+ |   | ███████╗╚██████╗╚██████╗███████╗███████╗███████║██╗     █████╔╝╚═╝ ██║╚██████╔╝ |   | 
+ |   | ╚══════╝ ╚═════╝ ╚═════╝╚══════╝╚══════╝╚══════╝╚═╝     ╚════╝     ╚═╝ ╚═════╝  |   | 
+ |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
+(_____)                                                                               (_____)
+Marcos Cevallos
+Development
+   ]]
+
+    return {
+      dashboard = {
+        enabled = true,
+        pane_gap = 1,
+        preset = {
+          header = logo,
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            {
+              icon = " ",
+              key = "c",
+              desc = "Config",
+              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+        },
+        sections = {
+          { section = "header" },
+          { section = "keys", padding = 1, gap = 1 },
+          { section = "startup" },
+        },
+      },
+      explorer = {
+        enabled = true,
+        replace_netrw = true,
+      },
+      indent = {
+        indent = {
+          only_scope = false,
+          enabled = true,
+          hl = { "SnacksIndent1", "SnacksIndent2", "SnacksIndent3", "SnacksIndent4" },
+        },
+        scope = {
+          enabled = true,
+        },
+        chunk = { enabled = true, only_current = true },
+      },
+      picker = {
+        enabled = true,
+        layout = {
+          preset = function()
+            return vim.o.columns >= 120 and "telescope" or "vertical"
+          end,
+        },
+        formatters = {
+          file = {
+            filename_first = true,
+          },
+        },
+        ---@class snacks.picker.sources.Config
+        sources = {
+          files = {
+            hidden = true,
+          },
+          explorer = {
+            hidden = true,
+            jump = {
+              close = false,
+            },
+            win = {
+              list = {
+                keys = {
+                  ["-"] = "explorer_up",
+                  ["<c-c>"] = "",
+                },
+              },
+            },
+          },
+        },
+        icons = {
+          ui = {
+            hidden = "",
+            live = "󰍉",
+          },
+        },
+        win = {
+          input = {
+            keys = {
+              ["<c-h>"] = false,
+              ["<c-l>"] = false,
+              ["<c-j>"] = { "preview_scroll_left", mode = { "i", "n" } },
+              ["<c-k>"] = { "preview_scroll_right", mode = { "i", "n" } },
+            },
+          },
+        },
+      },
+      image = {
+        enabled = true,
+      },
+    }
+  end,
   config = function(_, opts)
     require("snacks").setup(opts)
 
@@ -32,4 +142,47 @@ return {
     Snacks.toggle.zoom():map("<leader>wm"):map("<leader>uZ")
     Snacks.toggle.zen():map("<leader>uz")
   end,
+  keys = {
+    {
+      "<leader>E",
+      function()
+        local snacks = require("snacks")
+        snacks.picker.explorer()
+      end,
+      mode = { "n" },
+      desc = "Open the file explorer",
+    },
+    {
+      "<leader>,",
+      function()
+        local snacks = require("snacks")
+        snacks.picker.buffers()
+      end,
+      desc = "Buffers",
+    },
+    {
+      "<leader>/",
+      function()
+        local snacks = require("snacks")
+        snacks.picker.pick("grep")
+      end,
+      desc = "Grep (Root Dir)",
+    },
+    {
+      "<leader><space>",
+      function()
+        local snacks = require("snacks")
+        snacks.picker.pick("files")
+      end,
+      desc = "Find Files (Root Dir)",
+    },
+    {
+      "<leader>n",
+      function()
+        local snacks = require("snacks")
+        snacks.picker.notifications()
+      end,
+      desc = "Notification History",
+    },
+  },
 }
