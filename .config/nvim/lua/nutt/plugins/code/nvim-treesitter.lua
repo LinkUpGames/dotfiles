8e93b6bd8e55cbcb0d0bd8d0bf33681de468df1b
Anonymous <mjcevallos007@gmail.com>
2025-09-14 01:20:23 -0400

    working on nvim tree sitter

---
+++ .config/nvim/lua/nutt/plugins/code/nvim-treesitter.lua
@@ -0,0 +1,44 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = vim.fn.argc(-1) == 0,
+  build = ":TSUpdate",
+  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+  event = { "VeryLazy" },
+  main = "nvim-treesitter.configs",
+  ---@type TSConfig
+  opts = {
+    ensure_installed = { "bash", "html", "lua", "luadoc", "markdown", "markdown_inline" },
+    auto_install = true,
+    highlight = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
+    textobjects = {
+      move = {
+        enable = true,
+        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+      },
+    },
+  },
+  keys = {
+    { "<c-space>", desc = "Increment Selection" },
+    { "<bs>", desc = "Decrement Selection", mode = "x" },
+  },
+  opts_extend = {
+    "ensure_installed",
+  },
+}
