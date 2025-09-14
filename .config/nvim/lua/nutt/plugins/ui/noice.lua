1ffc69421b4b3d0829e06391cc8345ff9d879233
Anonymous <mjcevallos007@gmail.com>
2025-09-13 23:03:43 -0400

    updates

--- .config/nvim/lua/nutt/plugins/ui/noice.lua
+++ .config/nvim/lua/nutt/plugins/ui/noice.lua
@@ -1,43 +1,43 @@
 return {
-	"folke/noice.nvim",
-	dependencies = {
-		"rcarriga/nvim-notify",
-		-- "MunifTanjim/nui.nvim",
-	},
-	event = "VeryLazy",
-	opts = {
-		lsp = {
-			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-			},
-		},
-		-- you can enable a preset for easier configuration
-		presets = {
-			bottom_search = true, -- use a classic bottom cmdline for search
-			command_palette = true, -- position the cmdline and popupmenu together
-			long_message_to_split = true, -- long messages will be sent to a split
-			inc_rename = false, -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = false, -- add a border to hover docs and signature help
-		},
-	},
-	config = function(_, opts)
-		if vim.o.filetype == "lazy" then
-			vim.cmd([[messages clear]])
-		end
+  "folke/noice.nvim",
+  dependencies = {
+    -- "rcarriga/nvim-notify",
+    "MunifTanjim/nui.nvim",
+  },
+  event = "VeryLazy",
+  opts = {
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      },
+    },
+    -- you can enable a preset for easier configuration
+    presets = {
+      bottom_search = true, -- use a classic bottom cmdline for search
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false, -- add a border to hover docs and signature help
+    },
+  },
+  config = function(_, opts)
+    if vim.o.filetype == "lazy" then
+      vim.cmd([[messages clear]])
+    end
 
-		require("noice").setup(opts)
-	end,
-	keys = {
-		{ "<leader>sn", "", desc = "+noice" },
-		{
-			"<leader>snh",
-			function()
-				require("noice").cmd("history")
-			end,
-			desc = "Noice History",
-		},
-	},
+    require("noice").setup(opts)
+  end,
+  keys = {
+    { "<leader>sn", "", desc = "+noice" },
+    {
+      "<leader>snh",
+      function()
+        require("noice").cmd("history")
+      end,
+      desc = "Noice History",
+    },
+  },
 }
