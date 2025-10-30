ef877ea8c3d4d888d7d904a04dc9d9bc9ff5aa42
Anonymous <mjcevallos007@gmail.com>
2025-10-29 21:29:00 -0400

    Updated some more things

--- .config/nvim/lua/nutt/plugins/code/nvim-lspconfig.lua
+++ .config/nvim/lua/nutt/plugins/code/nvim-lspconfig.lua
@@ -216,81 +216,55 @@ return {
       opts.capabilities or {}
     )
 
+    servers["*"] = vim.tbl_deep_extend("force", servers["*"] or {}, {
+      capabilities = capabilities,
+    })
+
     -- Fetch all servers available through mason-lspconfig
     local mlsp = require("mason-lspconfig")
-    local all_mslp_servers = vim.tbl_keys(require("mason-lspconfig.mappings").get_mason_map().lspconfig_to_package)
+    local mason_all = vim.tbl_keys(require("mason-lspconfig.mappings").get_mason_map().lspconfig_to_package)
 
-    -- Which one's don't start with mason
-    local exclude_automatic_enable = {} ---@type string[]
+    -- Exclude from mason
+    local mason_exclude = {} ---@type string[]
 
     ---Configure a server given it's name
     ---@param server string
     ---@return boolean status Was the server successfully configured?
     local function configure(server)
-      --- Get the capabilities for the current lsp running
-      local server_opts = vim.tbl_deep_extend("force", {
-        capabilities = vim.deepcopy(capabilities),
-      }, servers[server] or {})
+      if server == "*" then
+        return false
+      end
 
-      -- If we defined the setup for this server than run it
-      if opts.setup[server] then
-        local run = opts.setup[server]
+      local sopts = opts.servers[server]
+      sopts = sopts == true and {} or (not sopts) and { enabled = false } or sopts
 
-        if run(server, server_opts) then
-          return true
-        end
-      elseif opts.setup["*"] then
-        local run = opts.setup["*"]
+      if sopts.enabled == false then
+        mason_exclude[#mason_exclude + 1] = server
 
-        if run(server, server_opts) then
-          return true
+        return false
+      end
+
+      local use_mason = sopts.mason ~= false and vim.tbl_contains(mason_all, server)
+      local setup = opts.setup[server] or opts.setup["*"]
+
+      if setup and setup(server, sopts) then
+        mason_exclude[#mason_exclude + 1] = server
+      else
+        vim.lsp.config(server, sopts) -- configure the server
+
+        if not use_mason then
+          vim.lsp.enable(server)
         end
       end
 
-      -- Configure language server
-      vim.lsp.config(server, server_opts)
-
-      -- manually enble if mason is false or if this is a server that cannot be installed with mason-lspconfig
-      if server_opts.mason == false or not vim.tbl_contains(all_mslp_servers, server) then
-        vim.lsp.enable(server)
-
-        return true
-      end
-
-      return false
+      return use_mason
     end
 
     -- Run installation for both local and mason lsp configs
-    local ensure_installed = {} ---@type string[]
-    for server, server_opts in pairs(servers) do
-      if server_opts then
-        server_opts = server_opts == true and {} or server_opts
-
-        if server_opts.enabled ~= false then
-          local configured = configure(server)
-
-          if configured then
-            -- Don't rerun the configuration
-            exclude_automatic_enable[#exclude_automatic_enable + 1] = server
-          else
-            ensure_installed[#ensure_installed + 1] = server
-          end
-        else
-          -- We don't want to run this
-          exclude_automatic_enable[#exclude_automatic_enable + 1] = server
-        end
-      end
-    end
-
-    -- Mason Config
-    local setup_config = {
-      -- Make sure to add those that we want to install separately by mason
-      ensure_installed = vim.tbl_deep_extend("force", ensure_installed, {}),
-      automatic_enable = {
-        exclude = exclude_automatic_enable,
-      },
-    }
-
-    mlsp.setup(setup_config)
+    local install = vim.tbl_filter(configure, vim.tbl_keys(servers))
+    mlsp.setup({
+      ensure_installed = install,
+      automatic_enable = { exclude = mason_exclude },
+    })
   end,
 }
