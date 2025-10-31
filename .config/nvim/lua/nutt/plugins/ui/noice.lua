07a4f2db166fcda4bfb3a3b25877ec6ae2bec7a0
marcosFIF <marcosc@fillitforward.com>
2025-10-31 08:30:01 -0400

    noice update

--- .config/nvim/lua/nutt/plugins/ui/noice.lua
+++ .config/nvim/lua/nutt/plugins/ui/noice.lua
@@ -14,6 +14,15 @@ return {
         ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
       },
     },
+    routes = {
+      {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      },
+    },
     -- you can enable a preset for easier configuration
     presets = {
       bottom_search = true, -- use a classic bottom cmdline for search
