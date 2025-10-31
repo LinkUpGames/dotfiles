a18c16d945993e05bb6a4c44847e3b32789d5c70
Anonymous <mjcevallos007@gmail.com>
2025-10-31 15:38:44 -0400

    adeed gitsigns

---
+++ .config/nvim/lua/nutt/plugins/ui/gitsigns.lua
@@ -0,0 +1,24 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {},
+  keys = {
+    {
+      "<leader>gsd",
+      "<cmd>Gitsigns toggle_word_diff<cr>",
+      mode = "n",
+      desc = "Toggle intra-line word-diff",
+    },
+    {
+      "<leader>gsp",
+      "<cmd>Gitsigns toggle_current_line_blame<cr>",
+      mode = "n",
+      desc = "Toggle line blame",
+    },
+    {
+      "<leader>gsh",
+      "<cmd>Gitsigns preview_hunk<cr>",
+      mode = "n",
+      desc = "Preview hunk",
+    },
+  },
+}
