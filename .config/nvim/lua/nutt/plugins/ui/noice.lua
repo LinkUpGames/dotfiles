3fd6f286841e85257fca4e372ec3e5e7889dfe54
marcosFIF <marcosc@fillitforward.com>
2025-11-03 10:47:14 -0500

    Added the lsp hover document thing with noice

--- .config/nvim/lua/nutt/plugins/ui/noice.lua
+++ .config/nvim/lua/nutt/plugins/ui/noice.lua
@@ -48,5 +48,29 @@ return {
       end,
       desc = "Noice History",
     },
+    {
+      "<C-f>",
+      function()
+        if not require("noice.lsp").scroll(4) then
+          return "<c-f>"
+        end
+      end,
+      silent = true,
+      expr = true,
+      desc = "Scroll Forward",
+      mode = { "i", "n", "s" },
+    },
+    {
+      "<C-b>",
+      function()
+        if not require("noice.lsp").scroll(-4) then
+          return "<c-b>"
+        end
+      end,
+      silent = true,
+      expr = true,
+      desc = "Scroll Backward",
+      mode = { "i", "n", "s" },
+    },
   },
 }
