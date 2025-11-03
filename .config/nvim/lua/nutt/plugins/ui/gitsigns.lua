cd262677325e0a940db5797e8b3c0376bf5a6108
mjcevallos007@gmail.com <devy@localhost.localdomain>
2025-11-02 21:10:05 -0500

    Added some more keybinds for git signs

--- .config/nvim/lua/nutt/plugins/ui/gitsigns.lua
+++ .config/nvim/lua/nutt/plugins/ui/gitsigns.lua
@@ -1,25 +1,53 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  event = { "BufReadPre", "BufNewFile", "BufWritePre" },
-  opts = {},
-  keys = {
-    {
-      "<leader>gsd",
-      "<cmd>Gitsigns toggle_word_diff<cr>",
-      mode = "n",
-      desc = "Toggle intra-line word-diff",
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile", "BufWritePre" },
+    opts = {},
+    keys = {
+      {
+        "<leader>gsd",
+        "<cmd>Gitsigns toggle_word_diff<cr>",
+        mode = "n",
+        desc = "Toggle intra-line word-diff",
+      },
+      {
+        "<leader>gsp",
+        "<cmd>Gitsigns toggle_current_line_blame<cr>",
+        mode = "n",
+        desc = "Toggle line blame",
+      },
+      {
+        "<leader>gsh",
+        "<cmd>Gitsigns preview_hunk<cr>",
+        mode = "n",
+        desc = "Preview hunk",
+      },
+      {
+        "<leader>gs]",
+        "<cmd>Gitsigns nav_hunk next<cr>",
+        mode = "n",
+        desc = "Next Hunk",
+      },
+      {
+        "<leader>gs[",
+        "<cmd>Gitsigns nav_hunk pre<cr>",
+        mode = "n",
+        desc = "Previous Hunk",
+      },
+      {
+        "<leader>gsr",
+        "<cmd>Gitsigns reset_hunk<cr>",
+        mode = { "n", "v" },
+        desc = "Reset Hunk",
+      },
     },
-    {
-      "<leader>gsp",
-      "<cmd>Gitsigns toggle_current_line_blame<cr>",
-      mode = "n",
-      desc = "Toggle line blame",
-    },
-    {
-      "<leader>gsh",
-      "<cmd>Gitsigns preview_hunk<cr>",
-      mode = "n",
-      desc = "Preview hunk",
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>gs", group = "Git Signs", icon = { icon = "", color = "cyan" } },
+      },
     },
   },
 }
