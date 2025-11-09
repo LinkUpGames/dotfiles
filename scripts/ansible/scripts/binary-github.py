cb3b854309196e4e4622e1c32872ede0cc87cf90
anonymous <mjcevallos007@gmail.com>
2025-11-09 18:51:29 -0500

    binary thingy

--- scripts/ansible/scripts/binary-github.py
+++ scripts/ansible/scripts/binary-github.py
@@ -1,6 +1,9 @@
 import requests
 import re
 import sys
+import tarfile
+import os
+import zipfile
 
 BASE_URL = "https://api.github.com/repos"
 
@@ -62,6 +65,24 @@ def get_release_file(url, filename):
     return filename
 
 
+def unzip_file(zip):
+    """
+    Unzip the file
+    """
+    with zipfile.ZipFile(zip, "r") as file:
+        file.extractall()
+    os.remove(zip)
+
+
+def untar_file(tar):
+    """
+    Untar the file
+    """
+    with tarfile.open(tar, "r:gz") as file:
+        file.extractall()
+        os.remove(tar)
+
+
 def main():
     """
     Entrance
@@ -69,18 +90,27 @@ def main():
     repo = ""
     filename = ""
     keywords = ""
+    binary = 0  # - | 1 | 2 -> 0 is binary, 1 is zip and 2 is tar
 
     try:
         repo = sys.argv[1]
         filename = sys.argv[2]
         keywords = sys.argv[3].split(",")
+        binary = {"binary": 0, "zip": 1, "tar": 2}.get(sys.argv[4].lower(), 0)
     except Exception:
-        print("Ex: python3 binary-github.py [repo] [filename] ['keyword1,keyword2']")
+        print(
+            "Ex: python3 binary-github.py [repo] [filename] ['keyword1,keyword2'] ['zip' | 'binary' | 'tar']"
+        )
         quit(1)
 
     releases = get_releases(repo)
     url = get_release(keywords, releases)
     file = get_release_file(url, filename or "")
+
+    if binary == 2:
+        untar_file(file)
+    elif binary == 1:
+        unzip_file(file)
 
 
 main()
