750a44dd6acdba4ecada93ec98b283b909c36a97
anonymous <mjcevallos007@gmail.com>
2025-11-12 21:24:54 -0500

    oki

--- .config/nvim/lua/nutt/plugins/ui/tiny-inline-diagnostics.lua
+++ .config/nvim/lua/nutt/plugins/ui/tiny-inline-diagnostics.lua
@@ -4,19 +4,32 @@ return {
   event = "VeryLazy",
   config = function()
     require("tiny-inline-diagnostic").setup({
-      transparent_bg = true,
+      transparent_bg = false,
       transparent_cursorline = true,
+      hi = {
+        mixing_color = "None",
+      },
       options = {
+        softwrap = 20,
+        set_arrow_to_diag_color = true,
+        show_source = {
+          if_many = true,
+        },
         add_messages = {
           display_count = true,
         },
         multilines = {
           enabled = true,
           trim_whitespaces = true,
+          -- always_show = true,
         },
         break_line = {
           enabled = true,
         },
+        overflow = {
+          padding = 5,
+        },
+        override_open_float = true,
       },
     })
 
