73295da68159bd194f47854e38a0df4d7abbe9e8
anonymous <mjcevallos007@gmail.com>
2025-11-14 23:16:10 -0500

    Updated some more thingies

--- scripts/ansible/.lazy.lua
+++ scripts/ansible/.lazy.lua
@@ -1,15 +1,32 @@
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "yaml" },
-	callback = function()
-		vim.bo.filetype = "yaml.ansible"
-	end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	pattern = { "yaml" },
+-- 	callback = function()
+-- 		vim.bo.filetype = "yaml.ansible"
+-- 	end,
+-- })
 
 return {
-	"stevearc/conform.nvim",
-	opts = {
-		formatters_by_ft = {
-			yaml = { lsp_format = "prefer" },
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				yaml = { lsp_format = "prefer" },
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			---@module "nutt.plugins.code.nvim-lspconfig"
+			---@type table<string, LspServerConfig>
+			servers = {
+				yamlls = {
+					filetypes = { "yaml", "yaml.ansible" },
+				},
+				ansiblels = {
+					filetypes = { "yaml", "yaml.ansible" },
+				},
+			},
 		},
 	},
 }
