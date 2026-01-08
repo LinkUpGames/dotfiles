eff1d718f735b2273cbda8e974b50a3274a30917
Anonymous <mjcevallos007@gmail.com>
2026-01-07 20:31:11 -0500

    using codediff for diffing changes

--- .config/nvim/lua/nutt/plugins/editor/diffview.lua
+++ .config/nvim/lua/nutt/plugins/editor/diffview.lua
@@ -1,3 +1,6 @@
+if true then
+  return {}
+end
 return {
   {
     "sindrets/diffview.nvim",
--- .config/nvim/lua/nutt/plugins/editor/vscode-diff.lua
+++
@@ -1,9 +0,0 @@
--- Don't install it for now until the diff system is out
-if true then
-  return {}
-end
-
-return {
-  "esmuellert/vscode-diff.nvim",
-  branch = "next",
-}
