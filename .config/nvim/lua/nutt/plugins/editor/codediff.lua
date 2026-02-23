59082e8071b483edf6bdd3b69228d84d5c506b03
Anonymous <mjcevallos@proton.me>
2026-02-23 07:04:19 -0500

    updated some thingies for codediff

--- .config/nvim/lua/nutt/plugins/editor/codediff.lua
+++ .config/nvim/lua/nutt/plugins/editor/codediff.lua
@@ -6,7 +6,47 @@ return {
     "MunifTanjim/nui.nvim",
   },
   cmd = "CodeDiff",
-  opts = {},
+  opts = function()
+    -- Disable cursor line on diff mode and tabline
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "CodeDiffOpen",
+      callback = function()
+        -- disable cursorline
+        local wins = vim.api.nvim_tabpage_list_wins(0)
+
+        for _, win in ipairs(wins) do
+          vim.wo[win].cursorline = false
+        end
+
+        -- disable status bar
+        vim.g.codediff_saved_showtabline = vim.o.showtabline
+        vim.o.showtabline = 0
+      end,
+    })
+
+    -- Enable tabline
+    -- vim.api.nvim_create_autocmd("User", {
+    --   pattern = "CodeDiffClose",
+    --   callback = function()
+    --     if vim.g.codediff_saved_showtabline then
+    --       vim.o.showtabline = vim.g.codediff_saved_showtabline
+    --       vim.g.codediff_saved_showtabline = nil
+    --     end
+    --   end,
+    -- })
+
+    return {
+      explorer = {
+        focus_on_select = true,
+      },
+      keymaps = {
+        view = {
+          toggle_explorer = "<leader>e",
+          focus_explorer = "<leader>b",
+        },
+      },
+    }
+  end,
   keys = {
     {
       "<leader>gd",
